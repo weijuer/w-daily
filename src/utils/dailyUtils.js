6418dbdec46f5d96d0a8ae0b1f3306f7ffb7cb29
Weijuer <ch_weijuer@163.com>
2021-11-16 18:40:23 +0800

    🎨 1.版本依赖升级；1.docs提交

--- src/utils/dailyUtils.js
+++ src/utils/dailyUtils.js
@@ -1,14 +1,15 @@
 // 引入daily下及其子目录下所有的.json文件
-const modules = import.meta.globEager('../daily/**/*.json');
+const modules = import.meta.glob('../../public/data/**/*.json');
+
+console.log(modules);
 
 export function generateDaily() {
   // 递归获取 views 文件夹下的所有.vue文件
-  return Object.entries(modules).reduce((daily, [fileName, { default: articles }]) => {
-    const name = fileName.match(/([0-9a-zA-Z\-]+)\.json$/)[1];
-    daily.push({
+  return Object.entries(modules).map(([fileName, articles]) => {
+    const [_, name] = fileName.match(/([0-9a-zA-Z\-]+)\.json$/);
+    return {
       name,
       articles,
-    });
-    return daily;
-  }, []);
+    };
+  });
 }
