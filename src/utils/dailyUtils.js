e337f249475776cac406a011a818f3386d0f618b
Weijuer <ch_weijuer@163.com>
2021-11-19 17:47:38 +0800

    ✨ 1.引入sql.js;

--- src/utils/dailyUtils.js
+++ src/utils/dailyUtils.js
@@ -1,11 +1,11 @@
 // 引入daily下及其子目录下所有的.json文件
-const modules = import.meta.glob('../../public/data/**/*.json');
+const modules = import.meta.globEager('/data/**/*.json');
 
 console.log(modules);
 
 export function generateDaily() {
   // 递归获取 views 文件夹下的所有.vue文件
-  return Object.entries(modules).map(([fileName, articles]) => {
+  return Object.entries(modules).map(([fileName, { default: articles }]) => {
     const [_, name] = fileName.match(/([0-9a-zA-Z\-]+)\.json$/);
     return {
       name,
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import { name } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: `/${name}/`,
   build: {
     outDir: 'docs',
   },
